e6c86e0bc79d4237b4dc3cb4a77e807fa72aca1a
shuli495 <xiaoyao495@163.com>
2024-04-10 23:04:17 +0800

    add tcp server

---
+++ src/main/java/com/pighand/aio/netty/MyServerInitializer.java
@@ -0,0 +1,17 @@
+package com.pighand.aio.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
+        pipeline.addLast(new MyServerHandler());
+    }
+}
